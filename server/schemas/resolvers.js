f97741befcb50339102a6b7198074f9054904280
smg061 <max.go.95@gmail.com>
2021-07-29 14:26:40 -0500

    more typedefs added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,50 +1,33 @@
-const {
-  createUser,
-  getSingleUser,
-  saveBook,
-  deleteBook,
-  login,
-} = require('../controllers/user-controller');
-
-
+const { createUser, getSingleUser, saveBook, deleteBook, login } = require("../controllers/user-controller");
+const {User} = require("../models");
 const resolvers = {
   Query: {
-    books: async () => {
-      return Thought.find().sort({ createdAt: -1 });
+    books: async (params) => {
+      return Book.find(params);
     },
-
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
+    user: async (parent, params) => {
+      return getSingleUser(params);
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("thoughts");
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 
-  // Mutation: {
-  //   addThought: async (parent, { thoughtText, thoughtAuthor }) => {
-  //     return Thought.create({ thoughtText, thoughtAuthor });
-  //   },
-  //   addComment: async (parent, { thoughtId, commentText }) => {
-  //     return Thought.findOneAndUpdate(
-  //       { _id: thoughtId },
-  //       {
-  //         $addToSet: { comments: { commentText } },
-  //       },
-  //       {
-  //         new: true,
-  //         runValidators: true,
-  //       }
-  //     );
-  //   },
-  //   removeThought: async (parent, { thoughtId }) => {
-  //     return Thought.findOneAndDelete({ _id: thoughtId });
-  //   },
-  //   removeComment: async (parent, { thoughtId, commentId }) => {
-  //     return Thought.findOneAndUpdate(
-  //       { _id: thoughtId },
-  //       { $pull: { comments: { _id: commentId } } },
-  //       { new: true }
-  //     );
-  //   },
-  // },
+  Mutation: {
+    addUser: async (parent, {body}) => {
+      return createUser(body);
+    },
+    login: async (parent, {body}) => {
+      return login(body);
+    },
+
+    saveBook: async(parent, {user, body}) => {
+      return saveBook(body);
+    }
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,16 +19,24 @@ const typeDefs = gql`
     savedBooks: [Book]
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
-    books: [Book]
-    users: [User]
+    books(username: String!): [Book]
+    user: User
+    me: User
+
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(authors: [String!], description: String!, bookId: String!, image: String!, link: String!, title: String!): Book
   }
 
 `;
-// type Mutation {
-//   addThought(thoughtText: String!, thoughtAuthor: String!): Thought
-//   addComment(thoughtId: ID!, commentText: String!): Thought
-//   removeThought(thoughtId: ID!): Thought
-//   removeComment(thoughtId: ID!, commentId: ID!): Thought
-// }
+
 module.exports = typeDefs;
